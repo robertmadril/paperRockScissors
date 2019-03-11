d1366795b479776c020782c782f8adc5b917fa4a
robertmadril <robertmmadril@gmail.com>
2019-03-10 18:45:15 -0700

    add pseudocode in app.js for game play

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,3 +10,13 @@
   firebase.initializeApp(config);
 
   var database = firebase.database();
+
+  /*
+
+  Both players join by inputting the name in their field.
+  They will only have access to one player side.
+  when player-one class on.click's the value will be sent to db. p2 will not see selection.
+  when player-two class on.click's the value will be sent to db and winner will display.
+  New game will start when both p1 and p2 have selected new game
+
+  */
